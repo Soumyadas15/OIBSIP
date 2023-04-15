817e3f1784aa43b59244a1edbc209dec44d10ca5
Saumyapratim Das <saumyadas2017@gmail.com>
2023-04-15 10:45:28 +0530

    New

--- van gogh/script.js
+++ van gogh/script.js
@@ -1,3 +1,6 @@
+// Please use Safari browser for a smooth experience
+
+
 $(window).scroll(function() {
     $(".content").css("opacity", 1 - $(window).scrollTop() / 550);
 });
@@ -6,22 +9,20 @@ $(window).scroll(function() {
     $(".content1").css("opacity", 1 - $(window).scrollTop() / 550);
 });
 
-var frame = 0;
-var speed = 500;
-var setHeight = document.getElementById("set-height")
-const vid = document.getElementById('video1'); 
 
-vid.addEventListener('loadedmetadata', function() {
-    setHeight.style.height = Math.floor(vid.duration) * speed + "px";
-});
-
-function animate(){  
-    var frame  = window.pageYOffset/speed;
-    if(vid.currentTime != frame) {
-        vid.currentTime  = frame;
+$(function() {
+    var setHeight = document.getElementById("set-height");
+    const vid = document.getElementById('video1');
+    var speed = 500;
+  
+    function update() {
+      setHeight.style.height = Math.floor(vid.duration) * speed + "px";
+      var frame = window.pageYOffset / speed;
+      if (vid.currentTime != frame) {
+        vid.currentTime = frame;
+      }
+      window.requestAnimationFrame(update);
     }
-    
-    window.requestAnimationFrame(animate);
-}
-
-window.requestAnimationFrame(animate);
+  
+    window.requestAnimationFrame(update);
+});
